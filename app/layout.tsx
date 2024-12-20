1072eeb14105bba0fd4c42629bb2df5d9fe8adb4
BooGu7 <boogu1823@gmail.com>
2024-12-20 23:03:26 +0700

    add speech-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,8 +24,11 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
-      <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
-        <Providers>{children}</Providers>
+      <body className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
+        <Providers>
+          {children}
+          <SpeedInsights />
+        </Providers>
       </body>
     </html>
   );
